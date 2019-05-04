b8669c8d18e3e83ad2892d2a37405c09d2a0c83c
Gabe Vazquez <gabe.vazquez@blackrock.com>
2019-05-04 23:30:22 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,12 @@
-def my_select(collection)
+def my_select(array)
  # your code here!
+  i = 0
+  selection = []
+  while i < array.length
+    if array[i]==true
+      selection << yield(array[i])
+      i+=1
+    end
+  end
+  selection
 end
